bb982be421c99f6a0a7140143062bead8cce92f4
sophiaSantosD <sophiasantos.code@gmail.com>
2025-11-20 22:22:37 -0300

    Exercício 010
    Ex010: Criar um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.

---
+++ MUNDO1/ex010.py
@@ -0,0 +1,4 @@
+# Conversor de Doláres 
+print('====== DESAFIO 10 ======')
+dinheiro = float(input('Quantos reais você tem na carteira? R$'))
+print('Com {:.2f} reais, você conseguirá comprar US${:.2f} dólares'.format(dinheiro, dinheiro/3,27))
