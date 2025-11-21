3aa43da451f688b5507d5ab23bd400386c6ff0af
sophiaSantosD <sophiasantos.code@gmail.com>
2025-11-20 22:20:15 -0300

    Exercício 008
    Ex008: Escrever um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.

---
+++ MUNDO1/ex008.py
@@ -0,0 +1,6 @@
+# Conversor de Medidas
+print('====== DESAFIO 08 ======')
+m = float(input('Digite um valor em metros: '))
+cm = m * 100
+mm = m * 1000
+print('O valor de {}m covertidos é {}cm e {}mm'.format(m, cm, mm))
