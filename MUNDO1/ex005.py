31d91ead62ad67ff40a1c61f5dc6b3e38080123a
sophiaSantosD <sophiasantos.code@gmail.com>
2025-11-20 22:16:56 -0300

    Exercício 005
    Ex005: Fazer um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.

---
+++ MUNDO1/ex005.py
@@ -0,0 +1,6 @@
+# Antecessor e Sucessor
+print('====== DESAFIO 05 ======')
+num = int(input('Digite um número: '))
+ant = num - 1
+suc = num + 1
+print('O sucessor e o antecessor do número {} são {} e {}'.format(num, ant, suc))
