8891d9e0d3759be0a0792658664eddbf0ffddafe
sophiaSantosD <sophiasantos.code@gmail.com>
2025-11-20 22:34:16 -0300

    Exercício 016
    Ex016: Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira.

---
+++ MUNDO1/ex016.py
@@ -0,0 +1,5 @@
+# Quebrando um número
+from math import trunc
+print('====== DESAFIO 16 ======')
+num = float(input('Digite um número: '))
+print('A parte inteira de {} é {}'.format(num, trunc(num)))
