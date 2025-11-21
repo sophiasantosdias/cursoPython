efd0e1f8eff59c9997f4981b26ba8b8d1fa5750c
sophiaSantosD <sophiasantos.code@gmail.com>
2025-11-20 22:14:06 -0300

    Exercício 003
    Ex003: Criar um programa que leia dois números e mostre a soma entre eles.

---
+++ MUNDO1/ex003.py
@@ -0,0 +1,6 @@
+# Soma de 2 números
+print('====== DESAFIO 03 ======')
+n1 = int(input('número 1: '))
+n2 = int(input('número 2: '))
+s = n1 + n2
+print('A soma entre {} e {} vale {}'.format(n1, n2, s))
