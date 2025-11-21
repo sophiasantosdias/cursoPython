3472b290eaddce2ab2e0f69a122b281043f07b25
sophiaSantosD <sophiasantos.code@gmail.com>
2025-11-20 22:26:55 -0300

    Exercício 013
    Ex013: Fazer um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.

---
+++ MUNDO1/ex013.py
@@ -0,0 +1,4 @@
+# Salário com 15% de aumento
+print('====== DESAFIO 13 ======')
+sal = float(input('Digite o salário inicial: R$'))
+print('O salário R${:.2f} com 15% de aumento será R${:.2f100}'.format(sal, sal*1.15))
