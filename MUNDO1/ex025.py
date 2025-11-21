ae1fb3cfa2af613b58b688c8394ee4fd8a8a78d2
sophiaSantosD <sophiasantos.code@gmail.com>
2025-11-20 22:48:58 -0300

    Exercício 025
    Ex025: Crie um programa que leia o nome de uma pessoa e diga se ela tem “SILVA” no nome.

---
+++ MUNDO1/ex025.py
@@ -0,0 +1,4 @@
+# Procurando uma string dentro da outra
+print('====== DESAFIO 25 ======')
+nome = input('Qual o seu nome? ').upper().strip()
+print('O seu nome tem Silva? {}'.format('SILVA' in nome))
