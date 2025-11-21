918d30f2f28339f1105e05458fb0e4ddf4d80169
sophiaSantosD <sophiasantos.code@gmail.com>
2025-11-21 12:34:51 -0300

    Exercício 038
    Ex038: Escreva um programa que leia dois números inteiros e compare-os.

---
+++ MUNDO2/ex038.py
@@ -0,0 +1,11 @@
+# Comparando números
+print('====== DESAFIO 38 ======')
+a = int(input('Primeiro número: '))
+b = int(input('Segundo número: '))
+
+if a > b:
+    print('O PRIMEIRO valor é maior')
+elif a == b:
+    print('Os valores são IGUAIS')
+else:
+    print('O SEGUNDO valor é maior')
