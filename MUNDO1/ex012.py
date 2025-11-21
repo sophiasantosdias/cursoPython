0207319e41efe1129d007434f31f82457c95f443
sophiaSantosD <sophiasantos.code@gmail.com>
2025-11-20 22:25:11 -0300

    Exercício 012
    Ex012: Fazer um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.

---
+++ MUNDO1/ex012.py
@@ -0,0 +1,4 @@
+# Produto com 5% de desconto
+print('====== DESAFIO 12 ======')
+valor = float(input('Qual o valor do produto? R$'))
+print('Depois do desconto de 5%, o valor do produto vai de {:.2f} para {:.2f}'.format(valor, valor*0.95))
