5fcbdef40df153608e4ccdf2da92c963cde292d4
sophiaSantosD <sophiasantos.code@gmail.com>
2025-11-20 22:28:26 -0300

    Exercício 014
    Ex014: Escrever um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.

---
+++ MUNDO1/ex014.py
@@ -0,0 +1,5 @@
+# Ceusius para Fahrenheit
+print('====== DESAFIO 14 ======')
+c = float(input('Digite a temperatura em C: '))
+f = 1.8*c + 32
+print('A temperatura {:.1f}°C corresponde a {:.1f}F.'.format(c, f))
