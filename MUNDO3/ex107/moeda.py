8d67e5f177b732cf6011ee88f3146d0e2cdc80ae
sophiaSantosD <sophiasantos.code@gmail.com>
2025-12-01 22:09:58 -0300

    Exercício 107
    Ex107: Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() e metade(). Faça também um programa que importe esse módulo e use algumas dessas funções.

---
+++ MUNDO3/ex107/moeda.py
@@ -0,0 +1,18 @@
+def metade(p):
+    met = p / 2
+    return met
+
+
+def dobro(p):
+    dobro = 2 * p
+    return dobro
+
+
+def aumento(p, v):
+    aum = p + p * v /100
+    return aum
+
+
+def diminuicao(p, v):
+    dim = p - p * v / 100
+    return dim
